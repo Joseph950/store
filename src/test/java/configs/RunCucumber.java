7752fa8836c5607ba8c548c744e145d6f0648c5d
Jose Mendieta <mendietacruzjose@gmail.com>
2026-06-08 22:01:22 -0600

    Se modificó el plugin del RunCucumber de pretty a html

--- src/test/java/configs/RunCucumber.java
+++ src/test/java/configs/RunCucumber.java
@@ -13,7 +13,7 @@ import reports.GenerateReport;
         features = "src\\test\\resources\\features",
         glue = "steps",
         plugin = {
-                "pretty",
+                "html",
                 "html:target/cucumber-reports/index.html",
                 "json:target/cucumber-reports/cucumber.json",
                 "junit:target/cucumber-reports/cucumber.xml"
