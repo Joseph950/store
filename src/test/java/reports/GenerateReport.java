26ed2dc60943c8198cb3c869e0fbdcf7dd2ad3ce
Jose Mendieta <mendietacruzjose@gmail.com>
2026-06-08 21:02:27 -0600

    Se modificó el runner de Cucumber y se agregaron opciones para generar el reporte final cucumber

---
+++ src/test/java/reports/GenerateReport.java
@@ -0,0 +1,29 @@
+package reports;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateReport {
+
+    public static void generate() {
+        System.out.println("SE ESTÁ GENERANDO EL REPORTE");
+
+        File reportOutputDirectory = new File("target/advanced-report");
+
+        List<String> jsonFiles = new ArrayList<>();
+        jsonFiles.add("target/cucumber-reports/cucumber.json");
+
+        Configuration configuration =
+                new Configuration(reportOutputDirectory,
+                        "Proyecto Selenium Cucumber");
+
+        ReportBuilder reportBuilder =
+                new ReportBuilder(jsonFiles, configuration);
+
+        reportBuilder.generateReports();
+    }
+}
